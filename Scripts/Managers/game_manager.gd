ce8bae5a9a6c44bad857a7c9f99006819c57c74e
Guilherme De Sá Pinto <guilhermepinto@ipvc.pt>
2025-04-01 22:43:19 +0100

    Finished boss lvl design and setup

--- Scripts/Managers/game_manager.gd
+++ Scripts/Managers/game_manager.gd
@@ -4,6 +4,7 @@ var pause_menu_screen: PackedScene = preload("res://Scenes/UI/pause_menu.tscn")
 
 func start_game():
 	EventsManager.set_lives_count(3.0)
+	EventsManager.set_slam_counter(3.0)
 	SceneManager.transisiton_scene("level_1", true)
 
 func back_to_title_screen():
